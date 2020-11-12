312bc07d302c4c00461a39a16d19f8d7ec0d83a5
Jonathan Romero <n.bjhon@gmail.co>
2020-11-12 16:50:24 -0500

    añadiendo autenticacion de login

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,10 @@
 from flask import render_template, session, redirect, flash, url_for
+from flask_login import login_user, login_required, logout_user
 
 from app.forms import LoginForm
 from . import auth
-
+from app.firestore_service import get_user
+from app.models import UserData, UserModel
 
 @auth.route("/login", methods=['GET', 'POST'])
 def login():
@@ -12,11 +14,42 @@ def login():
     }
 
     if login_form.validate_on_submit():
+
         username = login_form.username.data
-        session['username'] = username
+        password = login_form.password.data
     
-        flash('Nombre se usuario registrado con éxito!')
+        user_doc = get_user(username)
+
+        if user_doc.to_dict() is not None:
+            password_from_db = user_doc.to_dict()['password']
+
+            if password == password_from_db:
+                user_data = UserData(username, password)
+                user = UserModel(user_data)
+
+                login_user(user)
+
+                flash('Bienvenido de nuevo')
+
+                redirect(url_for('hello'))
+
+            else:
+                flash('La informacion no coincide')
+
+        else:
+            flash('El usuario no existe')
+
+        
     
         return redirect(url_for('index'))
 
     return render_template('login.html', **context)
+
+@auth.route('logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Regresa pronto')
+
+    return redirect(url_for('auth.login'))
+    
