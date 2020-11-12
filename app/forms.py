40cd27a1e5ea5f089b333e9ce78e09e94c0f3b56
Jonathan Romero <n.bjhon@gmail.co>
2020-11-12 18:36:22 -0500

    eliminar tareas

--- app/forms.py
+++ app/forms.py
@@ -6,3 +6,11 @@ class LoginForm(FlaskForm):
     username = StringField('Nombre de usuario', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Enviar')
+
+class TodoForm(FlaskForm):
+    description = StringField('Descripcion', validators=[DataRequired()])
+    submit = SubmitField('Crear')
+
+class DeleteTodoForm(FlaskForm):
+    submit = SubmitField('Borrar')
+
